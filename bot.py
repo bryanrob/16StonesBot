334c6bdb0830a5dbbec331a68a51a92ce04a0e8c
bryanrob <bryanrob@kean.edu>
2022-02-17 10:40:19 -0500

    Progress on implementing a fully-functioning game instance.

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 import os
 import random
 from Stones import SixteenStones
+from Instance import Instance, Player
 import discord
 from dotenv import load_dotenv
 
@@ -14,6 +15,8 @@ file.close()
 #TOKEN = os.getenv(token)
 
 client = discord.Client()
+
+instances={}
 
 @client.event
 async def on_ready():
@@ -48,17 +51,37 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content == '!play':
-        game = SixteenStones()
-        outputString =('Turn: '+str(game.getTurn()) + '\n')
-        currentBoard = game.getBoard()
-        for i in range(len(currentBoard)):
-            for j in range(currentBoard[i]):
-                outputString+='O '
-            outputString+='\n'
+    if message.content.startswith('!play'):
+        player1=Player(message.author.id,message.author.username)
+        outputString="**An unexpected error has occurred.**\nPlease make sure that the command syntax is correct."
+        splitMessage=message.content.split(" ")
+
+        if len(splitMessage==2):
+            player2=Player(client.get_user(splitMessage[1]).id,client.get_user(splitMessage[1]).username)
+
+            if player1.id in instances:
+                outputString="**Error:** @"+player1.id+" is already in an instance."
+            elif player2.id in instances:
+                outputString="**Error:** @"+player2.id+" is already in an instance."
+            else:
+                instance=Instance(player1,player2)
+                instances[player1.id]=instance
+                instances[player2.id]=instance
+
+                outputString=instance.outputString
+        else:
+            outputString="**Input error:** Ping the user who you want to play against."
+
+        #game = SixteenStones()
+        #outputString =('Turn: '+str(game.getTurn()) + '\n')
+        #currentBoard = game.getBoard()
+        #for i in range(len(currentBoard)):
+        #    for j in range(currentBoard[i]):
+        #        outputString+='O '
+        #    outputString+='\n'
         await message.channel.send(outputString)
 
-    elif message.content=='!take':
+    elif message.content.startswith('!take'):
         print("This should be configured to take in arguments.  String should contain '!take', not have only '!take'.")
 
         await message.channel.send("**'!take'** command received without any arguments.\nSorry, but this command is still in the works.")
